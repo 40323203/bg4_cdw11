f8e5d14f2a41ebecc18128f3a9fef48f42503e24
40323203 <40323203@gm.nfu.edu.tw>
2016-05-26 23:50:54 +0800

    new all

--- myflaskapp.py
+++ myflaskapp.py
@@ -7,19 +7,11 @@ import os
 import init
 
 #bg4
-
 import users.b.g4.b40323201_cdw11
-<<<<<<< HEAD
+import users.b.g4.bg4_40323202
 import users.b.g4.bg4_40323208
 import users.b.g4.bg4_test
-import users.b.g4.bg4_40323202
 
-=======
-import users.b.g4.bg4_40323208
-import users.b.g4.bg4_test
-import users.b.g4.bg4_40323202
-import users.b.g4.bg4_40323208
->>>>>>> f9a961fbf606a8c4e2360b689b8686377a644254
 
 # 確定程式檔案所在目錄, 在 Windows 有最後的反斜線
 _curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -391,17 +383,8 @@ if __name__ == "__main__":
     app.run()
 
 #bg4
-
-app.register_blueprint(users.b.g4.b40323201_cdw11.b40323201)
-<<<<<<< HEAD
-=======
+app.register_blueprint(users.b.g4.b40323201_cdw11.b40323201_cdw11)
+app.register_blueprint(users.b.g4.bg4_40323202.bg4_40323202)
 app.register_blueprint(users.b.g4.bg4_40323208.bg4_40323208)
 app.register_blueprint(users.b.g4.bg4_test.bg4_test)
-app.register_blueprint(users.b.g4.bg4_40323202.bg4_40323202)
->>>>>>> f9a961fbf606a8c4e2360b689b8686377a644254
-app.register_blueprint(users.b.g4.bg4_40323208.bg4_40323208)
-app.register_blueprint(users.b.g4.bg4_test.bg4_test)
-app.register_blueprint(users.b.g4.bg4_40323202.bg4_40323202)
-
-
 
