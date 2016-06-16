0c0af8cb233ecdca129c2a9f75fdfd7d7ff121dd
40323205 <40323205@gm.nfu.edu.tw>
2016-06-16 10:54:33 +0800

    20160616_40323205

--- myflaskapp.py
+++ myflaskapp.py
@@ -7,7 +7,6 @@ import os
 import init
 
 #bg4
-import users.b.g4.bg4_b40323201_cdw11
 import users.b.g4.bg4_40323202
 import users.b.g4.bg4_40323208
 import users.b.g4.bg4_40323205
@@ -385,7 +384,6 @@ if __name__ == "__main__":
     app.run()
 
 #bg4
-app.register_blueprint(users.b.g4.b40323201_cdw11.b40323201)
 app.register_blueprint(users.b.g4.bg4_40323202.bg4_40323202)
 app.register_blueprint(users.b.g4.bg4_40323208.bg4_40323208)
 app.register_blueprint(users.b.g4.bg4_40323205.bg4_40323205)
