73ea3b8e4e2c3d0ca6f45ba8491d48b541492d2b
40323208 <40323208@gm.nfu.edu.tw>
2016-06-16 12:12:11 +0800

    new 13 and add 15

--- users/b/g4/bg4_40323208.py
+++ users/b/g4/bg4_40323208.py
@@ -882,9 +882,9 @@ def eighteenthirty(x,y):
     x = 50
     y = 0
     degree = 20
-    first_degree = 20.78
-    startx = -233.06+100
-    starty = 49.48
+    first_degree =112
+    startx = -233.06+210
+    starty = -100
     repeat = 360 / degree
     # 先畫出左邊第一關鍵節
     outstring = '''
@@ -905,10 +905,10 @@ x1, y1 = mychain.basic_rot('''+str(startx)+","+str(starty)+", "+str(first_degree
     # 接著處理右邊的非虛擬鍊條
     # 先畫出右邊第一關鍵節
  
-    p = -17.89+100
-    k = 93.98
+    p =  -233.06+165
+    k = 120
     degree = 12
-    first_degree = 4.78
+    first_degree = 95
     repeat = 360 / degree
     # 第1節不是 virtual chain
     outstring += '''
@@ -926,7 +926,7 @@ p1, k1 = mychain.basic_rot('''+str(p)+","+str(k)+", "+str(first_degree)+''')
  
     # 上段連接直線
     # 從 x1, y1 作為起點
-    first_degree = 10.78
+    first_degree = 100.5
     repeat = 10
     outstring += '''
 m1, n1 = mychain.basic_rot(x1, y1, '''+str(first_degree)+''')
@@ -936,7 +936,7 @@ m1, n1 = mychain.basic_rot(x1, y1, '''+str(first_degree)+''')
  
     # 下段連接直線
     # 從 x11, y11 作為起點
-    first_degree = -10.78
+    first_degree = 79.4
     repeat = 10
     outstring += '''
 r1, s1 = mychain.basic_rot(x11, y11, '''+str(first_degree)+''')
